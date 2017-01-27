903c9943ad0fa917f2976705f837f7cff19e75b9
Nash Reilly <nrc.reilly@gmail.com>
2017-01-27 17:59:56 -0500

    finished error function and get num lib headers; started out on ch04/examples/copy.c; added sharedlib.txt for some ideas on how to compile/link to get_num.h and error_functions.h

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -27,7 +27,7 @@ static void outputError(Boolean useErr, int err, Boolean flushStdout,
 #define BUF_SIZE 500
     char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];  
 
-    vsnprintf(userMsg, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
+    vsnprintf(userMsg, BUF_SIZE, format, ap);
 
     if (useErr)
         snprintf(errText, BUF_SIZE, " [%s %s]",
@@ -44,4 +44,91 @@ static void outputError(Boolean useErr, int err, Boolean flushStdout,
     fflush(stderr);
 }
 
+void errMsg(const char *format, ...)
+{
+    va_list argList;
+    int savedErrno;
+
+    savedErrno = errno;
+
+    va_start(argList, format);
+    outputError(TRUE, errno, TRUE, format, argList);
+    va_end(argList);
+
+    errno = savedErrno;
+}
+
+void errExit(const char *format, ...)
+{
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errno, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
+}
+
+void err_exit(const char *format, ...)
+{
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errno, FALSE, format, argList);
+    va_end(argList);
+
+    terminate(FALSE);
+}
+
+void errExitEN(int errnum, const char *format, ...)
+{
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(TRUE, errnum, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
+}
+
+void fatal(const char *format, ...)
+{
+    va_list argList;
+
+    va_start(argList, format);
+    outputError(FALSE, 0, TRUE, format, argList);
+    va_end(argList);
+
+    terminate(TRUE);
+}
+
+void usageErr(const char *format, ...)
+{
+    va_list argList;
+
+    fflush(stdout);
+
+    fprintf(stderr, "Usage: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);
+    exit(EXIT_FAILURE);
+}
+
+void cmdLineErr(const char *format, ...)
+{
+    va_list argList;
+
+    fflush(stdout);
+
+    fprintf(stderr, "Command-line usage error: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);
+    exit(EXIT_FAILURE);
+}
 
