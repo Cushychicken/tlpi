be80e028583121d5ea1c4b26192b4def9213637f
Nash Reilly <nrc.reilly@gmail.com>
2017-02-02 13:57:22 -0500

    Fixed issue with ptee: always used argv[1] as output filename, which caused problems with append functionality

--- ch04/exercises/ptee.c
+++ ch04/exercises/ptee.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,18 +9,26 @@
 int main(int argc, char *argv[])
 {
     int outputFd, openFlags, filePerms, numRead;  
+    int opt;
     char buf[BUFSIZ];
 
     if (argc < 2)
         usageErr("%s new-file\n", argv[0]);
 
-    if ( (argv[1][0] == '-') && (argv[1][1] == 'a') )
-        openFlags = O_CREAT | O_WRONLY | O_APPEND;
-    else
-        openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
 
-    filePerms = S_IRUSR | S_IWUSR; 
-    outputFd = open(argv[1], openFlags, filePerms);
+    while ( (opt = getopt(argc, argv, "a")) != -1 ) {
+        switch (opt) {
+            case 'a':
+                openFlags = O_CREAT | O_WRONLY | O_APPEND;
+                break;
+            default: 
+                fprintf(stderr, "Usage %s [-a] name\n", argv[0]);
+        }
+    }
+
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
+    outputFd = open(argv[optind], openFlags, filePerms);
 
     while ( (numRead = read(STDIN_FILENO, &buf, BUFSIZ)) > 0 ) {
         printf("%s", buf);
